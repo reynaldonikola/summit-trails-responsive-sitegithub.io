2af8ab49c548082f0594cdea4535ac52e51a3f9b
reynaldonikola <reynaldonikolam@gmail.com>
2026-04-05 20:18:07 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-const toggleButton = document.getElementById('themeToggle');
-const themeId = 'theme-stylesheet';
-const themeHref = './css/theme.css';
+const toggleButton = document.getElementById("themeToggle");
+const themeId = "theme-stylesheet";
+const themeHref = "./css/theme.css";
 
 function enableTheme() {
   const existingTheme = document.getElementById(themeId);
 
   if (!existingTheme) {
-    const themeLink = document.createElement('link');
-    themeLink.rel = 'stylesheet';
+    const themeLink = document.createElement("link");
+    themeLink.rel = "stylesheet";
     themeLink.href = themeHref;
     themeLink.id = themeId;
     document.head.appendChild(themeLink);
   }
 
-  toggleButton.setAttribute('aria-pressed', 'true');
-  toggleButton.textContent = 'Original Theme';
+  toggleButton.setAttribute("aria-pressed", "true");
+  toggleButton.textContent = "Original Theme";
 }
 
 function disableTheme() {
@@ -24,12 +24,13 @@ function disableTheme() {
     existingTheme.remove();
   }
 
-  toggleButton.setAttribute('aria-pressed', 'false');
-  toggleButton.textContent = 'Switch Theme';
+  toggleButton.setAttribute("aria-pressed", "false");
+  toggleButton.textContent = "Switch Theme";
 }
 
-toggleButton.addEventListener('click', function () {
+toggleButton.addEventListener("click", function () {
   const themeIsActive = document.getElementById(themeId);
+
   if (themeIsActive) {
     disableTheme();
   } else {
